1a151ee3974137c73a17ca529b3c8464d6dd9bac
bedro96@naver.com <bedro96@naver.com>
2019-11-19 01:22:26 +0000

    Upload

--- vmss_ag.tf
+++ vmss_ag.tf
@@ -165,7 +165,7 @@ resource "azurerm_virtual_machine_scale_set" "terraformvmss" {
 
     settings = <<SETTINGS
     {
-        "fileUris": ["https://raw.githubusercontent.com/bedro96/manoj_vmss/master/setup.sh"],
+        "fileUris": ["https://raw.githubusercontent.com/bedro96/terraform_vmss_ag/master/setup.sh"],
         "commandToExecute": "/bin/bash ./setup.sh"
     }
     SETTINGS
