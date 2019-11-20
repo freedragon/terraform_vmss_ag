d7d77496be325b066d7a4db8a0c53e97a12a9dc3
Ji Yong Seong <jyseong@microsoft.com>
2019-11-20 14:01:44 +0900

    Update delete_vmss_instance.py
    adding self_delete.delete_vmss_instance()

--- delete_vmss_instance.py
+++ delete_vmss_instance.py
@@ -5,6 +5,7 @@ from InstanceMetadata import InstanceMetadata
 import requests, json, os, time, sys, socket
 
 from bearer_token import BearerAuth
+from self_delete import self_delete
 
 # Initializing InstanceMetadata
 metadata = InstanceMetadata().populate()
@@ -57,4 +58,5 @@ if (resp.status_code == 200):
                         logger.info("Delete " + host_name)
                         #check copying log and stopping custom metric
                         #delete vmss instance
+                        self_delete.delete_vmss_instance()
 
